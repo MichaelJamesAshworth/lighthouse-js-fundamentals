41710b6ccc54abcce568248127587e1f2fef30fe
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-17 20:21:34 +0000

    function that greets any name passed as a parameter

---
+++ hello.js
@@ -0,0 +1,16 @@
+/*
+const sayHello = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+//lets improve the functionality of this function
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");

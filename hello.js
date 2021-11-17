b56db03194a372f086f07db3645617c23a4ea111
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-17 20:29:48 +0000

    added additional examples of functions

--- hello.js
+++ hello.js
@@ -6,6 +6,7 @@ const sayHello = function () {
 sayHello();
 */
 
+/*
 //lets improve the functionality of this function
 const sayHello = function (name) {
   console.log("Hello, " + name);
@@ -14,3 +15,19 @@ const sayHello = function (name) {
 sayHello("caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+//console.log sample:
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+//return sample:
+//Note that a return statement will not ouput anything to the console
+//However, when a function returns a value, we can still console.log it later
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting); 
